21bc0d3f036ff29feafd37d89e9077cc72885f0c
saadjavaid67 <saadjavaid67@gmail.com>
2022-04-09 12:44:39 +0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,20 +80,20 @@
             </div>
         </nav> --}}
         <div id="wrapper" class="active">
-            <div id="sidebar-wrapper" class="">
-                <ul id="sidebar_menu" class="sidebar-nav">
+            <div id="sidebar-wrapper" class="pt-3">
+                <ul id="sidebar_menu" class="sidebar-nav border-bottom">
                     <li>
-
-                        
-                        <div class="d-flex">
-                               <img class="me-4 logo" src="{{ asset('assets/logo/logo.png') }}" alt="logo">
-                            {{ config('app.name', 'Laravel') }}
+                        <a>
+                            <div>
+                                <img class="logo" src="{{ asset('assets/logo/logo.png') }}" alt="logo">
+                                <span>{{ config('app.name', 'Laravel') }}</span>
+                            </div>
                             <img src="{{ asset('assets/navbar-toggler.png') }}" alt="">
-                           </div>
+                        </a>
 
                     </li>
                 </ul>
-                <ul class="sidebar-nav border-bottom py-5" id="sidebar">
+                <ul class="sidebar-nav border-bottom py-3" id="sidebar">
                     <li><a><i class="fas fa-border-all"></i><span>Home</span></a></li>
                     <li><a><i class="fas fa-message"></i><span>Contacts</span></a></li>
                     <li><a><i class="fas fa-user-group"></i><span>Members</span></a></li>
